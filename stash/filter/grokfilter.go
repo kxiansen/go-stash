e2a63398ba6d0557687a703e932e18d5e10b0601
Bing Kuang <bing.kuang@cootek.cn>
2021-11-05 14:50:05 +0800

    v1

--- stash/filter/grokfilter.go
+++ stash/filter/grokfilter.go
@@ -26,7 +26,7 @@ func GrokFilter(field, match_str string) FilterFunc {
 			fmt.Println(s)
 			fmt.Printf("match: \"%v\",groupNames: \"%v\",len(match): %d,len(groupNames): %d\n", match, groupNames, len(match), len(groupNames))
 			if err := recover(); err != nil {
-				fmt.Println(err) //这里的err其实就是panic传入的内容，"bug"
+				fmt.Println("[error]: ", err) //这里的err其实就是panic传入的内容，"bug"
 			}
 			fmt.Println("--------------------------------------------------------------------------------------")
 		}()
