6c54fd25d1dcd06e7089961906b5aed97c45eacc
Bing Kuang <bing.kuang@cootek.cn>
2021-11-16 11:49:50 +0800

    xx

--- stash/filter/mutatefilter.go
+++ stash/filter/mutatefilter.go
@@ -11,23 +11,19 @@ func MutateFilter(Add_fields [][]string) FilterFunc {
 			field_name := field[0]
 			value := field[1]
 			if strings.Contains(value, "%{") {
-				re := regexp.MustCompile(`%{(?P<field1>\S+)}%(?P<str1>[\s\S]*)%{(?P<field2>\S+)}%`)
-				match := re.FindStringSubmatch(value)
-				if len(match) == 4 {
-					v := m[match[1]].(string) + match[2] + m[match[3]].(string)
-					m[field_name] = v
-				} else if len(match) == 3 {
-					v := m[match[1]].(string) + match[2]
-					m[field_name] = v
-				} else if len(match) == 2 {
-					v := m[match[1]].(string)
-					m[field_name] = v
+				for {
+					re := regexp.MustCompile(`.*%{\s*(?P<d_field>\S+)\s*}%.*`)
+					match := re.FindStringSubmatch(value)
+					// groupNames := re.SubexpNames()
+					if len(match) >= 2 {
+						// v := m[match[1]].(string) + match[2] + m[match[3]].(string)
+						value = re.ReplaceAllString(value, m[match[1]].(string))
+					} else {
+						break
+					}
 				}
-
-			} else {
-				m[field_name] = value
 			}
-
+			m[field_name] = value
 		}
 		return m
 	}
