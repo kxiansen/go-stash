ec9b7125b2ac157cb39f433e9e357cf85bdc1d4a
Bing Kuang <bing.kuang@cootek.cn>
2021-11-16 15:24:50 +0800

    xx

--- stash/filter/mutatefilter.go
+++ stash/filter/mutatefilter.go
@@ -1,6 +1,7 @@
 package filter
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 )
@@ -18,6 +19,7 @@ func MutateFilter(Add_fields [][]string) FilterFunc {
 					if len(match) >= 2 {
 						found := re2.FindString(value)
 						if found != "" {
+							fmt.Println(found)
 							value = strings.Replace(value, found, m[match[1]].(string), 1)
 						}
 						// value = re2.ReplaceAllString(value, m[match[1]].(string))
