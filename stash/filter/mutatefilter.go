8ecb5db774e97e26df174cd32704d320eb692480
Bing Kuang <bing.kuang@cootek.cn>
2021-11-16 17:10:45 +0800

    xx

--- stash/filter/mutatefilter.go
+++ stash/filter/mutatefilter.go
@@ -14,7 +14,7 @@ func MutateFilter(Add_fields [][]string) FilterFunc {
 			value := field[1]
 			if strings.Contains(value, "%{") {
 				for {
-					re1 := regexp.MustCompile(`.*?%{\s*?(?P<d_field>\S+)\s*?}%.*`)
+					re1 := regexp.MustCompile(`.*%{\s*?(?P<d_field>\S+)\s*?}%.*`)
 					re2 := regexp.MustCompile(`%{\s*?\S+\s*?}%?`)
 					match := re1.FindStringSubmatch(value)
 					if len(match) >= 2 {
